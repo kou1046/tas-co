04bab39f0fb2543681d10ea3e74b1d1982dea067
kou1046 <iwashiro0517@yahoo.co.jp>
2023-02-12 08:22:09 +0900

    初期設定ファイルの追加

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./pages/**/*.{js,ts,tsx}", "./lib/components/**/*.{js,ts,tsx,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
